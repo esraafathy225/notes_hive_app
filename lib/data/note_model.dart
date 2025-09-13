871ed47355f2e529937384e7a092fba64aef8bdc
esraa.fathy225 <esraa.fathy225@gmail.com>
2025-09-13 15:09:50 +0300

    Edit Note model

--- lib/data/note_model.dart
+++ lib/data/note_model.dart
@@ -1,8 +1,16 @@
-class Note {
 
+import 'package:hive/hive.dart';
+
+@HiveType(typeId: 0)
+class Note extends HiveObject{
+
+  @HiveField(0)
   final int? id;
+  @HiveField(1)
   final String title;
+  @HiveField(2)
   final String content;
+  @HiveField(3)
   final String date;
 
   Note({
